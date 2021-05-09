a778b87e4c75cf9f7cf8cb99b7da00cd5e977f78
Jerry <GerardoGonzalezCode@gmail.com>
2021-05-09 18:53:49 -0500

    commit v1.0.6

--- routes/users.js
+++ routes/users.js
@@ -21,4 +21,24 @@ router.get('/', async(req, res) => {
   }
 });
 
+
+//get(this.newsApiUrl + `top-headlines?country=${country}` + this.apiKeyQueryParam)
+/* GET users listing. */
+router.get('/country', async(req, res) => {
+  try {
+    const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=${req.query.country}&apiKey=9231ede9f71f47af9db9da58a170aca6`)
+    res.send(newsAPI.data)
+  } catch (error) {
+    if(error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+  }
+});
+
 module.exports = router;
